629d13837a41e733a261b442c5d138535ee099d2
yanyanyaa <yclin3411@gmail.com>
2023-05-21 21:21:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 app.get('/', (req, res) => {
   res.send(`這是首頁`)
 })
